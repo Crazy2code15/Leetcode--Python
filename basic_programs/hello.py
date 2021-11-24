f7597558347e6e6e5c4adbbe646e6f493bef7b39
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2021-11-24 06:40:44 +0530

    Create hello.py

---
+++ basic_programs/hello.py
@@ -0,0 +1,9 @@
+# Author : Pratibha Singh 
+# This program prints the message
+
+def justPrint(text):
+    '''This function prints the text passed as argument to this function'''
+    print(text)
+
+if __name__ == '__main__':
+    justPrint('Hello')
