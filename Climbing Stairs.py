0d632a3e3cc655f8f010ba3b487c06a98737abcd
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2022-01-27 18:33:08 +0530

    Delete Climbing Stairs.py

--- Climbing Stairs.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def climbStairs(self, n: int, memo = {}) -> int:
-        if n in memo: return memo[n]
-        if n == 1: return 1 
-        if n == 2: return 2
-        if n == 3: return 3
-        memo[n] = self.climbStairs(n-1, memo) + self.climbStairs(n-2, memo)
-        return memo[n]
