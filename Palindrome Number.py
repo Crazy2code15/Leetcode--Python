7d8cf172c375fb164701bcf819e96ee9584e475c
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2022-01-27 18:33:43 +0530

    Delete Palindrome Number.py

--- Palindrome Number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome( self, x: int) -> bool:
-        n_str = str(x)
-        r_str = n_str[::-1]
-        if n_str == r_str:
-            return True
-        else:
-            False
